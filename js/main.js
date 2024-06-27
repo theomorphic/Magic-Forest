4ebb4bf6f1647fc26d7bc24be7f4794df89310e7
theomorphic <FelixMK@yandex.ru>
2024-06-27 21:07:08 +0500

    feat: add final magic

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 
 window.addEventListener("scroll", e=>{
-	document.body.style.cssText = `--scrollTop: ${this.scrollY}px`
+	document.body.style.cssText += `--scrollTop: ${this.scrollY}px`
+})
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+ScrollSmoother.create({
+	wrapper: ".wrapper",
+	content: ".content",
 })
